e68bfe0a25036716ce1bc02ba1245c540d955abb
Nicholas Yong <n.yong@yy4c4yq2yr.ad.deliveryhero.com>
2023-02-01 13:10:09 +0800

    Add hero section and profile picture

---
+++ components/hero/Hero.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Image from 'next/image';
+
+const Hero = () => {
+  return (
+    <section className="mx-auto w-11/12 max-w-7xl">
+      <h1>Why</h1>
+    </section>
+  );
+};
+
+export default Hero;
