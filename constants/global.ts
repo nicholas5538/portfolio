1529174e564b060bdc2f8344971678d5471acd4b
Nicholas Yong <n.yong@yy4c4yq2yr.ad.deliveryhero.com>
2023-02-05 22:08:02 +0800

    Add type and global constant for stagger animation

--- constants/global.ts
+++ constants/global.ts
@@ -11,3 +11,18 @@ export const externalLinks = {
   GitHub: 'https://github.com/nicholas5538',
   Instagram: 'https://www.instagram.com/friviolousnicholas',
 };
+
+export const childAnimation: {
+  [key: string]: { opacity: number; x: string | number };
+} = {
+  hidden: { opacity: 0, x: '-100%' },
+  show: { opacity: 1, x: 0 },
+};
+
+export const parentAnimation = {
+  ...childAnimation,
+  show: {
+    ...childAnimation.show,
+    transition: { type: 'spring', duration: 1, staggerChildren: 0.2 },
+  },
+};
