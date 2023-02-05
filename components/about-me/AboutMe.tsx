2583720766e9ee417bb43fff9539ce3a40511538
Nicholas Yong <n.yong@yy4c4yq2yr.ad.deliveryhero.com>
2023-02-05 22:08:49 +0800

    Implement framer motion, lazy loading on About Me section

--- components/about-me/AboutMe.tsx
+++ components/about-me/AboutMe.tsx
@@ -1,46 +1,66 @@
 'use client';
 
-import React from 'react';
-import Lottie from 'lottie-react';
+import React, { lazy } from 'react';
 import AboutMeLight from '../../public/animations/aboutme-light.json';
+import { LazyMotion, domAnimation, m } from 'framer-motion';
+import {
+  childAnimation as aboutMeBody,
+  parentAnimation as aboutMeMain,
+} from '@/constants/global';
+
+const Lottie = lazy(() => import('lottie-react'));
 
 const AboutMe = () => {
   return (
     <section className="relative z-0 bg-dark-blue">
-      <div className="aboutmeContainer">
-        <div className="md:col-span-2 md:justify-self-start lg:col-span-1">
-          <h2 className="text-blue text-xl font-bold mb-5 md:mb-0 md:text-3xl xl:text-4xl">
-            👨 About Me
-          </h2>
-          <h1 className="text-white-300 font-bold mb-5 md:mb-0">
-            Never too late to learn
-          </h1>
-        </div>
-        <h4 className="aboutmeText">
-          I always thought my dream was to be a licensed aircraft engineer, it
-          seemed like an appropriate dream given my education path, but I always
-          felt mundane.
-        </h4>
-        <h4 className="aboutmeText">
-          It all changed when I had my first and only programming module in
-          university, the fascination it produced when I wrote my 1st Hello
-          World program on Python.
-        </h4>
-        <h4 className="aboutmeText">
-          Since then, I have taken a liking in web development which comprises
-          of problem-solving skills and creativity.
-        </h4>
-        <h4 className="aboutmeText font-semibold m-0">
-          Graduated B.Eng. with Honours in Aircraft Systems Engineering, from{' '}
-          <a className="cursor-pointer text-light-blue no-underline hover:underline hover:underline-offset-4">
-            SIT
-          </a>
-          .
-        </h4>
-        <aside className="aboutmeAnimation">
-          <Lottie animationData={AboutMeLight} />
-        </aside>
-      </div>
+      <LazyMotion features={domAnimation}>
+        <m.div
+          variants={aboutMeMain}
+          initial="hidden"
+          whileInView="show"
+          viewport={{ once: true, amount: 0.025 }}
+          className="aboutmeContainer"
+        >
+          <m.div
+            variants={aboutMeBody}
+            className="md:col-span-2 md:justify-self-start"
+          >
+            <h2 className="text-blue text-xl font-bold mb-5 md:mb-0 md:text-3xl xl:text-4xl">
+              👨 About Me
+            </h2>
+            <h2 className="text-white-300 font-bold mb-5 md:mb-0 md:text-3xl xl:text-4xl">
+              Never too late to learn
+            </h2>
+          </m.div>
+          <m.h4 variants={aboutMeBody} className="aboutmeText">
+            I always thought my dream was to be a licensed aircraft engineer, it
+            seemed like an appropriate dream given my education path, but I
+            always felt mundane.
+          </m.h4>
+          <m.h4 variants={aboutMeBody} className="aboutmeText">
+            It all changed when I had my first and only programming module in
+            university, the fascination it produced when I wrote my 1st Hello
+            World program on Python.
+          </m.h4>
+          <m.h4 variants={aboutMeBody} className="aboutmeText">
+            Since then, I have taken a liking in web development which comprises
+            of problem-solving skills and creativity.
+          </m.h4>
+          <m.h4
+            variants={aboutMeBody}
+            className="aboutmeText font-semibold m-0"
+          >
+            Graduated B.Eng. with Honours in Aircraft Systems Engineering, from{' '}
+            <a className="cursor-pointer text-light-blue no-underline hover:underline hover:underline-offset-4">
+              SIT
+            </a>
+            .
+          </m.h4>
+          <m.aside variants={aboutMeBody} className="aboutmeAnimation">
+            <Lottie animationData={AboutMeLight} />
+          </m.aside>
+        </m.div>
+      </LazyMotion>
       <div className="dark-blue-gradient"></div>
     </section>
   );
