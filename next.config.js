a7b2741c5e0cb88756ea455d15bf41b8c0c80e83
nicholas5538 <nicholas5538@gmail.com>
2023-08-24 16:32:00 +0800

    Configure webpack options

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  devIndicators: {
+    buildActivityPosition: 'bottom-right',
+  },
+  eslint: {
+    ignoreDuringBuilds: false
+  },
   reactStrictMode: true,
+  output: 'standalone',
+  typescript: {
+    ignoreBuildErrors: false
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
