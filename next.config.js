7be8c59adbaf1f322c1d2496bb30253f5ba449e7
nicholas5538 <nicholas5538@gmail.com>
2023-09-16 12:02:39 +0800

    Add bundle analyzer and update dependencies

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
 const nextConfig = {
   compress: true,
   devIndicators: {
@@ -22,4 +26,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+module.exports = withBundleAnalyzer(nextConfig);
